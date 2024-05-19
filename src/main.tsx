f9534a3fdf3ffa812722be82b6411c29b1730da2
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-19 03:16:50 -0300

    styles: adding onest font

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// React
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Global styles
+import "./index.css";
+
+// Font
+import "@fontsource-variable/onest";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
